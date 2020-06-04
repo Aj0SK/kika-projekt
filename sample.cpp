62d1f51eb37a02d0443dcbb1e208e15fe5ec8731
KikaSK <korecovak@gmail.com>
2020-06-04 23:42:57 +0200

    Draw circle

--- sample.cpp
+++ sample.cpp
@@ -1,30 +1,75 @@
 #include "objLoad.h"
 #include "bmpWrite.h"
+#include "Vector.h"
 
 #include <iostream>
 #include <vector>
 #include <fstream>
+#include <cmath>
+
+using std::byte;
+using std::pair;
+
+std::vector<std::byte> data;
+
+void Color_pixel(int X, int Y, int w, byte R, byte G, byte B)
+{
+    data[3*Y*w+3*X] = B;
+    data[3*Y*w+3*X+1] = G;
+    data[3*Y*w+3*X+2] = R;
+}
+
+bool Is_inside(Vector AB, Vector BC, Vector CA, Vector AX, Vector BX, Vector CX)
+{
+    if (((AX^AB).z() > 0) && ((BX^BC).z() > 0) && ((CX^CA).z() > 0))
+        return true;
+    if (((AX^AB).z() < 0) && ((BX^BC).z() < 0) && ((CX^CA).z() < 0))
+        return true;
+    return false;
+}
+
+struct triangle
+{
+    triangle(Vector A, Vector B, Vector C) : A(A), B(B), C(C) 
+    {
+        AB = B-A;
+        BC = C-B;
+        CA = A-C;
+        BA = A-B;
+        CB = B-C;
+        AC = C-A;
+    };
+    
+    Vector A,B,C;
+    Vector AB, BA, AC, CA, BC, CB;
+    
+};
+
+struct circle
+{
+    circle(Vector S, int r) : S(S), r(r) {};
+    Vector S;
+    int r;
+};
 
 int main()
 {
-    std::vector<std::byte> data;
+    int W = 1024;
+    int H = 1024;
     
-    int W = 128;
-    int H = 128;
+    data.resize(3*W*H);
     
-    for(int i=0; i<H; ++i)
-        for(int j=0; j<W; ++j)
+    circle C({500,400},150);
+    
+    for(int i=0; i<W; ++i)
+        for(int j=0; j<H; ++j)
         {
-            if(i<H/4 && j<W/4)
+            Color_pixel(i, j, W, (byte)100,(byte)100,(byte)100);
+            Vector X(i,j);
+            if((X-C.S).size()<=C.r)
             {
-                data.push_back(static_cast<std::byte>(0));
-                data.push_back(static_cast<std::byte>(0));
-                data.push_back(static_cast<std::byte>(255));
-                continue;
+                Color_pixel(i, j, W, (byte)255, (byte)0, (byte)0);
             }
-            data.push_back(static_cast<std::byte>(255));
-            data.push_back(static_cast<std::byte>(0));
-            data.push_back(static_cast<std::byte>(0));
         }
     
     bmp::bmpWrite("testOut.bmp", W, H, data);
