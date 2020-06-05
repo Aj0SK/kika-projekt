7942a9858888c0392c99a68bbaa03cb073d9efb4
Aj0SK <andrejkorman@gmail.com>
2020-06-05 10:05:18 +0200

    Add moving circle

--- Makefile
+++ Makefile
@@ -5,13 +5,16 @@ SAMPLES = samples
 
 all: samples
 
-samples: writeBMPDemo loadMeshDemo sample1
+samples: writeBMPDemo loadMeshDemo sample1 sample2
 
 writeBMPDemo:
-	${CC} ${CFLAGS} -I $(SRC) $(SAMPLES)/writeBMPDemo.cpp && ./a.out
+	${CC} ${CFLAGS} -I $(SRC) $(SAMPLES)/writeBMPDemo.cpp
 
 loadMeshDemo:
-	${CC} ${CFLAGS} -I $(SRC) $(SAMPLES)/loadMeshDemo.cpp $(SRC)/objLoad.cpp && ./a.out
+	${CC} ${CFLAGS} -I $(SRC) $(SAMPLES)/loadMeshDemo.cpp $(SRC)/objLoad.cpp
 	
 sample1:
-	${CC} ${CFLAGS} -I $(SRC) $(SAMPLES)/sample.cpp $(SRC)/objLoad.cpp && ./a.out
+	${CC} ${CFLAGS} -I $(SRC) $(SAMPLES)/sample1.cpp $(SRC)/objLoad.cpp
+
+sample2:
+	${CC} ${CFLAGS} -I $(SRC) $(SAMPLES)/sample2.cpp $(SRC)/objLoad.cpp
---
+++ samples/sample2.cpp
@@ -0,0 +1,76 @@
+#include "objLoad.h"
+#include "bmpWrite.h"
+#include "Vector.h"
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <cmath>
+
+using std::byte;
+using std::pair;
+
+constexpr int kFrames = 600;
+constexpr int kW = 1024;
+constexpr int kH = 1024;
+
+std::vector<std::byte> data;
+
+void Color_pixel(int X, int Y, int w, byte R, byte G, byte B)
+{
+    data[3*Y*w+3*X] = B;
+    data[3*Y*w+3*X+1] = G;
+    data[3*Y*w+3*X+2] = R;
+}
+
+bool Is_inside(Vector AB, Vector BC, Vector CA, Vector AX, Vector BX, Vector CX)
+{
+    if (((AX^AB).z() > 0) && ((BX^BC).z() > 0) && ((CX^CA).z() > 0))
+        return true;
+    if (((AX^AB).z() < 0) && ((BX^BC).z() < 0) && ((CX^CA).z() < 0))
+        return true;
+    return false;
+}
+
+struct circle
+{
+    circle(Vector S, int r) : S(S), r(r) {};
+    Vector S;
+    int r;
+};
+
+
+void drawCircle(int x, int y, int r)
+{
+    circle C({x,y},r);
+    
+    for(int i=0; i<kW; ++i)
+        for(int j=0; j<kH; ++j)
+        {
+            Color_pixel(i, j, kW, (byte)100,(byte)100,(byte)100);
+            Vector X(i,j);
+            if((X-C.S).size()<=C.r)
+            {
+                Color_pixel(i, j, kW, (byte)255, (byte)0, (byte)0);
+            }
+        }
+    return;
+}
+
+int main()
+{
+    data.resize(3*kW*kH);
+    
+    int currx = 200, curry = 200;
+    
+    for(int f=0; f<kFrames; ++f)
+    {
+        drawCircle(currx, curry, kFrames);
+        currx += 1;
+        curry += 1;
+        bmp::bmpWrite("img-" + std::to_string(f) + ".bmp", kW, kH, data);
+    }
+    
+    return 0;
+}
